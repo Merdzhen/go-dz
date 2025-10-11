b73498c1769c2531a36e75ec843a990499da2081
Merdzhen <merdzhen.k@gmail.com>
2025-10-12 00:05:09 +0300

    4.12. Домашнее задание - Строки и функции

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,4 +7,18 @@ func main() {
 	const usdRub = 81.27
 	var eurUsd = usdRub / usdEur
 	fmt.Println(eurUsd)
+	sum, currencyFrom, currencyTo := scanData()
+	convert(sum, currencyFrom, currencyTo)
+}
+
+func convert(sum int, currencyFrom string, currencyTo string) int {
+	scanData()
+	return 0
+}
+
+func scanData() (int, string, string) {
+	var sum int
+	var currencyFrom, currencyTo string
+	fmt.Scan(&sum, &currencyFrom, &currencyTo)
+	return sum, currencyFrom, currencyTo
 }
