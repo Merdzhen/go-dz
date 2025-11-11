95d8271bc47dd9fb2761317eb1c2d5275ba07c42
Merdzhen <merdzhen.k@gmail.com>
2025-11-11 14:44:09 +0300

    11.16. os

--- 3-bin/storage/storage.go
+++ 3-bin/storage/storage.go
@@ -1,1 +1,42 @@
 package storage
+
+import (
+	"bin/bins"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func SaveBinsToFile(bins []bins.Bin, fileName string) error {
+	data, err := json.Marshal(bins)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadBinsFromFile(fileName string) ([]bins.Bin, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	var bins []bins.Bin
+	err = json.Unmarshal(data, &bins)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return bins, nil
+}
