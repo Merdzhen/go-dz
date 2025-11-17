1dd1bd30e8468d2f29a2a95b522906e18a24ef5e
Merdzhen <merdzhen.k@gmail.com>
2025-11-17 15:24:15 +0300

    12.16 Interfaces

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"bin/bins"
+	"bin/storage"
+)
 
+func main() {
+    myStorage := storage.NewFileStorage()
+    
+    bin := bins.NewBin("123", false, "test")
+		binList := bins.NewBinList([]bins.Bin{*bin})
+		
+    myStorage.SaveBins(binList.Bins, "data.json")
 }
